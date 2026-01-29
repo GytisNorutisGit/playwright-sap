e9054fd274d319b94fc6edc8d521c00052460dd1
Gytis Norutis <gytis.norutis@outlook.com>
2026-01-29 21:25:44 +0000

    refactoring to remove secrets

--- e2e/sap-4-hana.spec.ts
+++ e2e/sap-4-hana.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('Add Item to cart and then validate cart value', { tag: '@cart' }, async ({ page }) => {
-  await page.goto('https://my426318.s4hana.cloud.sap/ui?sap-language=EN&help-mixedLanguages=false&help-autoStartTour=PR_28A0E50CC77DAF91#Shell-home');
+  await page.goto(process.env.SAP_URL!);
   await page.getByRole('textbox', { name: 'Email, User ID or Login Name' }).click();
-  await page.getByRole('textbox', { name: 'Email, User ID or Login Name' }).fill('gytis.norutis@outlook.com');
+  await page.getByRole('textbox', { name: 'Email, User ID or Login Name' }).fill(process.env.SAP_EMAIL!);
   await page.getByRole('button', { name: 'Continue' }).click();
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('BallyGowan2026!');
+  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.SAP_PASSWORD!);
   await page.getByRole('button', { name: 'Continue' }).click();
   await page.waitForLoadState('networkidle');
   await page.getByRoleUI5('IconTabFilter', { text: ' Trial Center' }).click();
